792e7ba98acf266570c4b328c9823327782e7843
IBR41Furkan <71mmerceif@gmail.com>
2021-10-31 00:13:04 +0300

    Recommit

--- src/main/Sim.java
+++ src/main/Sim.java
@@ -28,13 +28,18 @@ public class Sim {
     public static Queue Customers;
     public static Random stream;
 
-    public static void main(String args[]) {
+    public static void main(String argv[]) {
 
         MeanInterArrivalTime = 4.5;
         MeanServiceTime = 3.2;
         SIGMA = 0.6;
+<<<<<<< HEAD
         TotalCustomers = 50;
         long seed = Long.parseLong(args[0]);
+=======
+        TotalCustomers = 1000;
+        long seed = Long.parseLong(argv[0]);
+>>>>>>> parent of f7891b3... New changes committed
         stream = new Random(seed); // initialize rng stream
         FutureEventList = new EventList();
         Customers = new Queue();
